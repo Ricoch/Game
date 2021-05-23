dbd73515e770560f2e1796d0f632b184c121e0b4
MathiMate <mathixlasa@gmail.com>
2021-05-23 16:03:19 -0300

    Changes2

--- Master.js
+++ Master.js
@@ -1,16 +1,12 @@
-window.addEventListener("load",inicio);
 
-function inicio() {
+/*     let grisVacio = document.querySelector('.Gris-parrafo-vacio')
+ */
+window.addEventListener("load",gris);
 
-    const Grissinfondo2 = document.querySelector
-
-    function fondo() {
-        return [im]
+function gris() {
+    let img = "<img src='/Game/Grissinfondo.png'>";
+    for (let i= 1; i <= 3; i++) {
+        img+=img;
     }
-
-    let im = <img src='Grissinfondo2.png'></img>; 
-    for (let im = 1; im <= 80; im++) {
-        im = <img src='Grissinfondo2.png'></img>;
-        return
-    }
+    document.querySelector(".Gris_parrafo").innerHTML = img;
 }
