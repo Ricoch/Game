431a538b1b9cd170dcdfdea36361565659609c46
MathiMate <mathixlasa@gmail.com>
2021-05-22 23:01:01 -0300

    Empezando2

---
+++ Master.js
@@ -0,0 +1,9 @@
+const Grissinfondo = document.querySelector
+
+function fondo() {
+    img = "<img src='Grissinfondo.png'>";
+    return [img]
+}
+for (let im = 1; im <= 80; im++) {
+    let fondo = img
+}
