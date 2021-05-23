f57156732e833b41d5e07772c18d19d065b9a55b
MathiMate <mathixlasa@gmail.com>
2021-05-22 23:17:46 -0300

    idk

--- Master.js
+++ Master.js
@@ -1,9 +1,9 @@
 const Grissinfondo = document.querySelector
 
 function fondo() {
-    img = "<img src='Grissinfondo.png'>";
+    img = <img src='Grissinfondo.png'></img>;
     return [img]
 }
 for (let im = 1; im <= 80; im++) {
-    let fondo = img
+    let fondo = im
 }
