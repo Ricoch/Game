c22514e63dc747a469362d87692d5f45f6cd1d9f
MathiMate <mathixlasa@gmail.com>
2021-05-30 19:49:19 -0300

    Changes3

--- Master.js
+++ Master.js
@@ -4,9 +4,9 @@
 window.addEventListener("load",gris);
 
 function gris() {
-    let img = "<img src='/Game/Grissinfondo.png'>";
-    for (let i= 1; i <= 3; i++) {
+    let img = "<img src='../Grissinfondo.png'>";
+    for (let i= 0; i < 6; i++) {
         img+=img;
     }
-    document.querySelector(".Gris_parrafo").innerHTML = img;
+    document.querySelector(".Gris").innerHTML = img;
 }
